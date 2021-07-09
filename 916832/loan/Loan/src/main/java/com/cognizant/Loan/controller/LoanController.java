53b6991fc6a8dc2cb5af39aa452ff54fc9757c34
sravanimaddela <sravanimaddela98@gmail.com>
2021-07-10 02:04:32 +0530

    Microservices Handson

---
+++ 916832/loan/Loan/src/main/java/com/cognizant/Loan/controller/LoanController.java
@@ -0,0 +1,18 @@
+package com.cognizant.Loan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+	
+
+	@GetMapping("/{number}")
+	public String getLoanByNumber(@PathVariable String number)
+	{
+		return "{ number: \"H00987987972342\", type: \"car\", loan: 400000, emi: 3258, tenure: 18 }";
+	}
+}
